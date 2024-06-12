a6e98f05b9bf4c6e0f2819e344352a087736e1ba
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-06-12 10:02:02 +0000

    Triggers! decrease and email validation

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+-- SQL script that creates trigger that decreases quant after adding a new item
+
+CREATE TRIGGER order_decrease
+BEFORE INSERT ON orders
+FOR EACH ROW UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
