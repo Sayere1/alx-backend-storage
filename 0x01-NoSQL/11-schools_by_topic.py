1f461c850dba118b1cbca1c496ffcb9ea0e3f48d
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-06-17 03:20:30 -0700

    updated

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Python function that returns the list of school having a specific topic.'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''mongo_collection = object, topic(str) will be topic searched'''
+    topic_filter = { 
+            'topics': { '$elemMatch': { '$eq': topic, }, },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
