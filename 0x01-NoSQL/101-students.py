d553d3853e54471ef3dea1d85cf8848c7df635c1
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-06-17 03:39:42 -0700

    update

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''Python function that returns all students sorted by average score.'''
+
+
+def top_students(mongo_collection):
+    '''mongo_collection will be the pymongo collection object'''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': { '_id': 1, 'name': 1, 'averageScore': {'$avg': {
+                            '$avg': '$topics.score', }, },
+                    'topics': 1, }, },
+            {
+                '$sort': {'averageScore': -1}, },
+        ]
+    )
+    return students
